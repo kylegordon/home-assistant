571733dfd5a2e6ef9ea26314778b65ea8a80487b
Kyle Gordon <kyle@lodge.glasgownet.com>
2017-11-18 22:54:55 +0000

    Initial idea

---
+++ homeassistant/components/sensor/fineoffset.py
@@ -0,0 +1,3 @@
+## Just a placeholder for an idea
+## Read WeatherStationFSK directly, via /dev/ttyUSB0
+## Like https://github.com/home-assistant/home-assistant/blob/master/homeassistant/components/sensor/lacrosse.py
